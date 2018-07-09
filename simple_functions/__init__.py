53e8ce9b5c6c6ce82e5cec2e3c29b0b8df2ce0d0
mrf3 <mrf345@gmail.com>
2018-07-09 22:52:00 +0800

    fixing commented print_something

--- simple_functions/__init__.py
+++ simple_functions/__init__.py
@@ -8,18 +8,17 @@ def print_something(input):
     return input
 
 
-# from time import sleep
-
 # def print_something_slowly(input, duration):
-#     """ To sleep, print and return input """
-#     if not isinstance(input, str):
-#         raise(
-#             TypeError(
-#                 "print_something_slowly(input=) must be string"))
-#     if not isinstance(duration, int):
-#         raise(
-#             TypeError(
-#                 "print_something_slowly(duration=) must be int"))
-#     sleep(duration)
-#     print(input)
-#     return input
+    """ To sleep, print and return input """
+    from time import sleep
+    if not isinstance(input, str):
+        raise(
+            TypeError(
+                "print_something_slowly(input=) must be string"))
+    if not isinstance(duration, int):
+        raise(
+            TypeError(
+                "print_something_slowly(duration=) must be int"))
+    sleep(duration)
+    print(input)
+    return input
